d04caf064654b4f70497d87c2488b1aadd98ae4b
aj <userone@netwobble.com>
2018-04-11 21:33:54 -0400

    Biz Article update

--- libs/biz-modules/biz-article/article/src/article-update-display/article-update-display.component.ts
+++ libs/biz-modules/biz-article/article/src/article-update-display/article-update-display.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Article } from '@abcfoundry/biz-modules/biz-article/article/src/+store/article-store.interfaces';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'abc-article-update-display',
@@ -14,9 +14,10 @@ export class ArticleUpdateDisplayComponent implements OnInit {
   @Output() onRemove = new EventEmitter<Article>();
 
   articleForm: FormGroup;
+  sections: FormArray ;
   articleId;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     // console.log('In Article-Read-Display ', this.article.id)
@@ -45,25 +46,42 @@ export class ArticleUpdateDisplayComponent implements OnInit {
       articleSection2 = this.article.section2;
     }
 
+    this.articleForm = this.formBuilder.group({
+      status: [ articleStatus , Validators.required],
+      updated: [articleUpdated],
+      title: [articleTitle],
+      summary: [articleSummary],
+      tags: [articleTags],
+      links: [articleLink],
+      sections: this.formBuilder.array([ this.createSection() ])
+    })
 
-    this.articleForm = new FormGroup({
+/*    if (this.editMode) {
+      //take care if IDs in Edit mode
+      this.articleForm.addControl('id', new FormControl(articleId, Validators.required));
+      // this.articleForm.addControl('articleId', new FormControl('', Validators.required));
+    }*/
+
+
+/*    this.articleForm = new FormGroup({
       status: new FormControl(articleStatus),
       updated: new FormControl(articleUpdated),
       title: new FormControl(articleTitle),
       summary: new FormControl(articleSummary),
       tags: new FormControl(articleTags),
       links: new FormControl(articleLink),
+      sections: new FormArray([]),
       section: new FormControl(articleSection),
       section2: new FormControl(articleSection2)
-    });
+    });*/
 
       //
+
       if (this.editMode) {
         //take care if IDs in Edit mode
         this.articleForm.addControl('id', new FormControl(articleId, Validators.required));
         // this.articleForm.addControl('articleId', new FormControl('', Validators.required));
-
-    }
+       }
 
 
   }
@@ -71,6 +89,29 @@ export class ArticleUpdateDisplayComponent implements OnInit {
   onSubmit() {
     console.log(this.articleForm);
   }
+
+
+  createSection(): FormGroup {
+    return this.formBuilder.group({
+      section: ['']
+    });
+  }
+
+  onAddSection() {
+
+    // control refers to your formarray
+    const control = <FormArray>this.articleForm.controls['sections'];
+    // add new formgroup
+    control.push(this.createSection());
+  }
+
+  onRemoveSection(index: number) {
+    // control refers to your formarray
+    const control = <FormArray>this.articleForm.controls['sections'];
+    // remove the chosen row
+    control.removeAt(index);
+  }
+
   /**
    * Tip: Utilize getters to keep templates clean
    */
